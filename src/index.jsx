fcd25fa1e3aad3a78d6dee83cc23b13bb6a9c16e
Cehesse <chatellet.steven@gmail.com>
2023-10-04 14:50:06 +0200

    ajout basename a router

--- src/index.jsx
+++ src/index.jsx
@@ -20,7 +20,7 @@ import Footer from "./layout/Footer"
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/Kasa">
       <div className="App">
         <Header />
           <Routes>
